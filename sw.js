33eb498c1652c94ed98b15c72739934caad07d4a
The Red Sunflower <32282891+theredsunflower@users.noreply.github.com>
2018-06-12 18:42:01 -0400

    Add files via upload
    added offline functionality

--- sw.js
+++ sw.js
@@ -2,8 +2,9 @@ self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open('v1').then(function(cache) {
       return cache.addAll([
-        '/'
-        '/index.html'
+        '/',
+        '/index.html', 
+        '/restaurant.html'
       ]);
     })
   );
