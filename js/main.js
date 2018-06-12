83e5aeac93fd5c1c65bc9aacecc22a9f2455519b
The Red Sunflower <32282891+theredsunflower@users.noreply.github.com>
2018-06-12 14:36:21 -0400

    Add files via upload
    registered service worker

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js')
+  .then(function(reg) {
+    // registration worked
+    console.log('Registration succeeded');
+  }).catch(function(error) {
+    // registration failed
+    console.log('Registration failed with ' + error);
+  });
+}
+
 let restaurants,
   neighborhoods,
   cuisines
