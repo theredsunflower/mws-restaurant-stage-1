988e59e7df968110fb80884db72fef83a8fc7b14
The Red Sunflower <32282891+theredsunflower@users.noreply.github.com>
2018-06-20 11:32:44 -0400

    Add files via upload
    added support for dynamic URLs

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@ self.addEventListener('install', function(event) {
       return cache.addAll([
         '/',
         '/index.html', 
-        '/restaurant.html'
+        '/restaurant.html?id={x}'
       ]);
     })
   );
